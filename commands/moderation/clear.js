9959cbbe3c59bcdc9621b7b8b5d0ca3f99bf958f
Krusty <maxime.godin59@gmail.com>
2020-03-24 13:55:44 +0100

    Add files via upload

---
+++ commands/moderation/clear.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: "clear",
+    aliases: ["purge", "nuke"],
+    category: "moderation",
+    description: "Clears the chat",
+    run: async (client, message, args) => {
+        if (message.deletable) {
+            message.delete();
+        }
+    
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("You can't delete messages....").then(m => m.delete(5000));
+        }
+
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.reply("Please enter the number of messages you want to delete.").then(m => m.delete(5000));
+        }
+
+        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("Sorryy... I can't delete messages.").then(m => m.delete(5000));
+        }
+
+        let deleteAmount;
+
+        if (parseInt(args[0]) > 100) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
+
+        message.channel.bulkDelete(deleteAmount, true)
+            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
+            .catch(err => message.reply(`Something went wrong... ${err}`));
+    }
+}
